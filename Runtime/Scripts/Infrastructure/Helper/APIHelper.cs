a1d792678acb9f667a553f0ad79717ce7661c81c
issui <mizigoku@gmail.com>
2025-03-06 02:26:21 +0900

    FunctionのAPI関連基盤の実装

---
+++ Runtime/Scripts/Infrastructure/Helper/APIHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace JABARACdesign.Base.Infrastructure.Helper
+{
+    /// <summary>
+    /// API関連のヘルパークラス。
+    /// </summary>
+    public static class APIHelper
+    {
+        /// <summary>HTTPメソッド</summary>
+        public enum HttpMethod
+        {
+            GET,
+
+            POST,
+
+            PUT,
+
+            DELETE,
+
+            NONE,
+        }
+    }
+}
---
+++ Runtime/Scripts/Infrastructure/Network/API/IAPIRequest.cs
@@ -0,0 +1,23 @@
+﻿using JABARACdesign.Base.Infrastructure.Helper;
+
+namespace JABARACdesign.Base.Infrastructure.Network.API
+{
+    /// <summary>
+    /// APIリクエストインターフェース。
+    /// </summary>
+    public interface IAPIRequest
+    {
+        string Uri { get; }
+
+        APIHelper.HttpMethod Method { get; }
+    }
+    
+    /// <summary>
+    /// APIリクエストインターフェース(リクエストDTO付き)。
+    /// </summary>
+    /// <typeparam name="T">リクエストDTO</typeparam>
+    public interface IApiRequest<T> : IAPIRequest
+    {
+        T Dto { get; }
+    }
+}
