b13170245a62b64cbb13a08ac47533735aeeb573
issui <mizigoku@gmail.com>
2025-04-02 01:01:01 +0900

    クエリパラメータへの変換インターフェースへの対応

---
+++ Runtime/Scripts/Infrastructure/Dto/API/IQueryParamConvertable.cs
@@ -0,0 +1,11 @@
+﻿namespace JABARACdesign.Base.Infrastructure.Dto.API
+{
+    /// <summary>
+    /// クエリパラメータ変換可能インターフェース。
+    /// </summary>
+    public interface IQueryParamConvertible
+    {
+        string ToQueryParams();
+    }
+
+}
