7f9af54bcf587da49acce2136163377ff32f363a
issui <mizigoku@gmail.com>
2025-06-10 06:17:49 +0900

    Storage保存におけるローカルパス指定の実装

--- Runtime/Scripts/Application/Interface/IPathProvider.cs
+++ Runtime/Scripts/Application/Interface/IPathProvider.cs
@@ -9,8 +9,17 @@ namespace JABARACdesign.Base.Application.Interface
         /// </summary>
         /// <param name="identifier">識別子(プロジェクトで定義したEnum)</param>
         /// <typeparam name="TEnum">Enum</typeparam>
-        /// <returns>指定した識別子に対応するファイルのフルパス</returns>
+        /// <returns>指定した識別子に対応するファイルのパス</returns>
         public string GetPath<TEnum>(TEnum identifier)
+            where TEnum : struct, Enum;
+        
+        /// <summary>
+        /// 指定した識別子に対応するローカルのパスを取得する。
+        /// </summary>
+        /// <param name="identifier">識別子(プロジェクトで定義したEnum)</param>
+        /// <typeparam name="TEnum">Enum</typeparam>
+        /// <returns>指定した識別子に対応するファイルのローカルのフルパス</returns>
+        public string GetLocalPath<TEnum>(TEnum identifier)
             where TEnum : struct, Enum;
     }
 }
